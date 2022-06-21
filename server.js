86f91889562d1a6dc9dec4b7548e31770875e670
dave <davidpascual200@gmail.com>
2022-06-20 20:30:22 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path')
+const fs = require('fs');
+const { get } = require('http');
+
+const apiRouter = require('/routes/')
+
+const app = express();
+
+//set port
+const PORT = process.env.port || 3001; //????
+
+//middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static('public'));
+
+//request
+// app.get('/', (req,res) => {   //show homepage
+//     res.sendFile(path.join(__dirname, './public/index.html'))
+// });
+
+// app.get('/notes', (req, res) => {
+//     console.info(`${req.method} request received`);
+//     res.json(notes)
+//     // res.sendFile(path.join(__dirname, './public/notes.html'))
+// });
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'))
+// });
+
+// app.get('/api/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, './db/db.json'))
+    
+// });
+
+// app.post('/api/notes', (req, res) => {
+//  console.info
+// });
+
+app.listen(PORT,() => 
+console.log(`App listening at https://localhost:${PORT} `)
+);
